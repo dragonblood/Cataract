85d55298b4eb51d02e4a28b78b8967d79fc4376a
Vipul <Petkar.vipul@gmail.com>
2020-04-02 01:48:28 +0530

    File Upload Added

---
+++ Cataract/views.py
@@ -0,0 +1,52 @@
+# libraries are imported
+import os
+# rendering the template using render
+from django.shortcuts import render
+# this will be used to get the path of the media folder from settings
+from django.conf import settings
+# for storing the file in the desired folder, FileSystemStorage is used  
+from django.core.files.storage import FileSystemStorage
+# this will handle MultiValueDictKeyError using try and except
+from django.utils.datastructures import MultiValueDictKeyError
+# using class from models.py
+from . models import file_storage
+from django.templatetags.static import static
+
+
+# def index(request):
+#     path = settings.MEDIA_ROOT
+#     print(path)
+#     img_list = os.listdir(path)
+#     context = {'images' : img_list}
+#     return render(request, "Cataract/Cataract.html", context)
+	
+def file_storage_to_db(request):
+	file_path = os.path.join(settings.CONTENT_DIR, 'assets/img')
+
+	if request.method == 'POST':
+		try:
+			myFile = request.FILES['myFile']
+		except MultiValueDictKeyError:
+			print("Error.......")
+			myFile = "None"
+		fs = FileSystemStorage(location=file_path)	
+		filename = fs.save(myFile.name, myFile)
+		file_url = fs.url(filename)
+		file_information = file_storage()
+		file_information.file_name = myFile.name
+		file_information.file_url = file_url 
+		file_information.save()
+		
+		context = {
+					'filename': myFile.name, 
+					'file_url': file_url
+				  }
+		for name in os.listdir(file_path):
+			if path != 'favicon.png' and path != 'LibrusLogo.png' and path != context['filename']:
+				p = "/".join([file_path,name])
+				os.remove(p)
+				
+		Cataract = '/img/' + context['filename']
+		return render(request, 'Cataract.html', {'context': Cataract})
+	else:
+		return render(request, 'Cataract.html')		

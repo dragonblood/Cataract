03d74998b0cfc1f0097df74671a427725e5027d9
Vipul <Petkar.vipul@gmail.com>
2020-04-12 15:21:19 +0530

    bad working

--- Cataract/views.py
+++ Cataract/views.py
@@ -1,5 +1,6 @@
 # libraries are imported
 import os
+import requests
 # rendering the template using render
 from django.shortcuts import render
 # this will be used to get the path of the media folder from settings
@@ -13,12 +14,7 @@ from . models import file_storage
 from django.templatetags.static import static
 
 
-# def index(request):
-#     path = settings.MEDIA_ROOT
-#     print(path)
-#     img_list = os.listdir(path)
-#     context = {'images' : img_list}
-#     return render(request, "Cataract/Cataract.html", context)
+
 	
 def file_storage_to_db(request):
 	file_path = os.path.join(settings.CONTENT_DIR, 'assets/img')
@@ -41,17 +37,24 @@ def file_storage_to_db(request):
 					'filename': myFile.name, 
 					'file_url': file_url
 				  }
-		predict(context['filename'])
+
+		image = '/'.join([file_path, context['filename']])
+		pred = predict(image)
 
 		for name in os.listdir(file_path):
 			if name != 'favicon.png' and name != 'LibrusLogo.png' and name != context['filename']:
 				p = "/".join([file_path,name])
 				os.remove(p)
-
-		Cataract = '/img/' + context['filename']
-		return render(request, 'Cataract.html', {'context': Cataract})
+		res = pred['result']['tags']
+		return render(request, 'Cataract.html', {'label':res, 'context':context['filename'] })
 	else:
 		return render(request, 'Cataract.html')		
 
 def predict(name):
-	pass
+	api_key = 'acc_77ab554b8ab01cf'
+	api_secret = '134439d363b2cad920b1c6bec288d7ae'
+	image_url = 'https://cdn.clippingpath.in/wp-content/uploads/2017/11/low-res.jpg'
+
+	response = requests.get('https://api.imagga.com/v2/tags?image_url=%s' % image_url, auth=(api_key, api_secret))
+	result = response.json()
+	return(result)
